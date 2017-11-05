ced0159e2909281e64fda4aaf2547d4227339298
Bezuhlyi.denys <denys.bezuhlyi@hneu.net>
2017-11-05 18:52:34 +0200

    Add project files.

---
+++ Aviacompany.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aviacompany.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
